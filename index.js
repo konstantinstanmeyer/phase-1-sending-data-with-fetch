b031fde44dcd3a5585ad79e27b05eea753b834c6
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-08-15 11:23:25 -0600

    commit tree 310d1d81a4e40839a7b1f298ef9191dc9436de02

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(name, email){
+    return fetch('http://localhost3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify( {
+            name,
+            email
+        } )
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        return document.innerHTML = object['id']
+    })
+
+}
